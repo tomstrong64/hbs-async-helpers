b53ead29110aa6ee73fc31f5b064324a38961781
tomstrong64 <tom.strong64@gmail.com>
2024-08-14 22:26:20 +0100

    Replace reassigned params and dangling underscores

--- index.js
+++ index.js
@@ -22,36 +22,37 @@ function asyncHelpers(hbs) {
     }
 
     appendToBuffer(source, location, explicit) {
+      let asyncSource;
       // Force a source as this simplifies the merge logic.
       if (!Array.isArray(source)) {
-        source = [source];
+        asyncSource = [source];
       }
-      source = this.source.wrap(source, location);
+      asyncSource = this.source.wrap(source, location);
 
       if (this.environment.isSimple) {
-        return ['return await ', source, ';'];
+        return ['return await ', asyncSource, ';'];
       }
       if (explicit) {
         // This is a case where the buffer operation occurs as a child of another
         // construct, generally braces. We have to explicitly output these buffer
         // operations to ensure that the emitted code goes in the correct location.
-        return ['buffer += await ', source, ';'];
+        return ['buffer += await ', asyncSource, ';'];
       }
-      source.appendToBuffer = true;
-      source.prepend('await ');
-      return source;
+      asyncSource.appendToBuffer = true;
+      asyncSource.prepend('await ');
+      return asyncSource;
     }
   };
   handlebars.JavaScriptCompiler = asyncCompiler;
 
-  const _compile = handlebars.compile;
-  const _template = handlebars.VM.template;
-  const _escapeExpression = handlebars.escapeExpression;
-  const escapeExpression = function (value) {
+  const { compile, escapeExpression } = handlebars;
+  const { template: vmTemplate } = handlebars.VM;
+
+  const asyncEscapeExpression = function (value) {
     if (isPromise(value)) {
-      return value.then((v) => _escapeExpression(v));
+      return value.then((v) => escapeExpression(v));
     }
-    return _escapeExpression(value);
+    return escapeExpression(value);
   };
 
   function lookupProperty(containerLookupProperty) {
@@ -64,36 +65,42 @@ function asyncHelpers(hbs) {
   }
 
   handlebars.template = function (spec) {
-    spec.main_d =
+    const asyncSpec = spec;
+    asyncSpec.main_d =
       (prog, props, container, depth, data, blockParams, depths) =>
       async (context) => {
         // const main = await spec.main
-        container.escapeExpression = escapeExpression;
-        container.lookupProperty = lookupProperty(container.lookupProperty);
+        const asyncContainer = container;
+        asyncContainer.escapeExpression = asyncEscapeExpression;
+        asyncContainer.lookupProperty = lookupProperty(
+          container.lookupProperty,
+        );
+
+        let asyncDepths = depths;
         if (depths.length == 0) {
-          depths = [data.root];
+          asyncDepths = [data.root];
         }
-        const v = spec.main(
-          container,
+        const v = asyncSpec.main(
+          asyncContainer,
           context,
           container.helpers,
           container.partials,
           data,
           blockParams,
-          depths,
+          asyncDepths,
         );
         return v;
       };
-    return _template(spec, handlebars);
+    return vmTemplate(asyncSpec, handlebars);
   };
 
   handlebars.compile = function (template, options) {
-    const compiled = _compile.apply(handlebars, [template, { ...options }]);
+    const compiled = compile.apply(handlebars, [template, { ...options }]);
 
     return function (context, execOptions) {
-      context = context || {};
+      const newContext = context || {};
 
-      return compiled.call(handlebars, context, execOptions);
+      return compiled.call(handlebars, newContext, execOptions);
     };
   };
   handlebars.ASYNC_VERSION = getPackageVersion();
